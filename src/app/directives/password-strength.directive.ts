2d80ecc6d2e0e0d9deb69c8528a386485989a5e7
radrabik <radoslav.drabik@gmail.com>
2022-04-10 10:12:55 +0200

    2 files modified

--- src/app/directives/password-strength.directive.ts
+++ src/app/directives/password-strength.directive.ts
@@ -1,10 +1,15 @@
 
 import {Directive} from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator} from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
 import { createPasswordStrengthValidator } from '../validators/password-strength.validator'
 
 @Directive({
-  selector: "[passwordStrength]"
+  selector: "[passwordStrength]",
+  providers: [{
+    provide: NG_VALIDATORS, 
+    useExisting: PasswordStrengthDirective,
+    multi:true
+  }]
 })
 export class PasswordStrengthDirective implements Validator {
 
